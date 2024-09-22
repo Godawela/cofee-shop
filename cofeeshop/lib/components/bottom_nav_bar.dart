a4dc35d40ce057882eb4469d5a5b2b7cc8b44d04
Godawela <godawelaus.21@uom.lk>
2024-09-22 19:45:57 +0530

    route pages

--- cofeeshop/lib/components/bottom_nav_bar.dart
+++ cofeeshop/lib/components/bottom_nav_bar.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class MyBottomNavBar extends StatelessWidget {
-  const MyBottomNavBar({super.key});
+  final void Function(int)? onTabChange;
+
+  MyBottomNavBar({super.key, required this.onTabChange,});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(25),
       child: GNav(
+        onTabChange: (value) => onTabChange!(value),
         color: Colors.grey[400],
         mainAxisAlignment: MainAxisAlignment.center,
         activeColor: Colors.grey[700],
@@ -17,11 +20,11 @@ class MyBottomNavBar extends StatelessWidget {
         tabActiveBorder: Border.all(color: Colors.white),
         tabs: const [
         GButton(icon: Icons.home,
-        text: 'Shop',
+        text: ' Shop',
         ),
 
         GButton(icon: Icons.shopping_bag_outlined,
-        text: 'Cart',
+        text: ' Cart',
         )
       ]),
     );
--- cofeeshop/lib/pages/home_page.dart
+++ cofeeshop/lib/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:cofeeshop/components/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 
+import 'shop_page.dart';
+import 'cart_page.dart';
+
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -9,12 +13,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+    int _selectedIndex = 0;
+    void navigateBottomBar(int index) {
+    setState(() {
+    _selectedIndex = index;
+});
+}
+
+// pages
+final List<Widget> _pages =[
+// shop page
+    ShopPage(),
+
+// cart page
+    CartPage(),
+];
+
+
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       backgroundColor: Colors.grey[300],
-      bottomNavigationBar: const MyBottomNavBar(),
-
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange:(index) => navigateBottomBar(index)
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
